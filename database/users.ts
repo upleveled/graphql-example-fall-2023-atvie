f1f5d7d1bd4a351324458066fbb9788debf66046
Victor Ejike Nwosu <ejikello123@yahoo.com>
2024-01-15 17:39:40 +0100

    Add logged in user query

--- database/users.ts
+++ database/users.ts
@@ -1,7 +1,27 @@
+import { cache } from 'react';
+import { Users } from '../migrations/00002-createTableUsers';
+import { sql } from './connect';
+
+export const getUserByFirstName = cache(async (firstName: string) => {
+  if (!firstName) {
+    return undefined;
+  }
+
+  const [user] = await sql<Users[]>`
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        first_name = ${firstName}
+  `;
+  return user;
+});
+
 export async function isUserAdminBySessionToken(
   sessionToken: string | undefined,
 ) {
   // FIXME: Implement proper authorization
-  if (sessionToken === 'macca') return await true;
+  if (sessionToken === 'Victor') return await true;
   return await false;
 }
