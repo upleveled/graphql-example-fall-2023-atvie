ba3415ab2bfd8ac18a9fa3ed96c89f3a56221b37
Victor Ejike Nwosu <74430629+Eprince-hub@users.noreply.github.com>
2024-01-19 17:02:02 +0100

    Update app/(auth)/login/page.tsx
    Co-authored-by: Karl Horky <karl.horky@gmail.com>

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import LoginForm from './LoginForm';
 
 export default function LoginPage() {
-  // FIXME: Make secure session token and rename insecureSessionTokenCookie to sessionToken
+  // FIXME: Create secure session token and rename insecureSessionTokenCookie to sessionToken everywhere
   const insecureSessionTokenCookie = cookies().get('sessionToken');
 
   if (insecureSessionTokenCookie?.value) {
