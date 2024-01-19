cdb3418a2f1079b2900e3ca47e27e25724176826
Victor Ejike Nwosu <ejikello123@yahoo.com>
2024-01-19 15:45:58 +0100

    Delete isUserAdminBySessionToken function

--- database/users.ts
+++ database/users.ts
@@ -25,11 +25,3 @@ export const getUserByInsecureSessionToken = cache(
     return user;
   },
 );
-
-export async function isUserAdminBySessionToken(
-  sessionToken: string | undefined,
-) {
-  // FIXME: Implement proper authorization
-  if (sessionToken === 'macca') return await true;
-  return await false;
-}
