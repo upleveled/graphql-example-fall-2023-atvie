b3f00d73b3f5824bb3ebfeb1a7ab4e112c34128c
Victor Ejike Nwosu <ejikello123@yahoo.com>
2024-01-19 11:54:09 +0100

    Remove failing image import

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,22 @@
 import Image from 'next/image';
-import smilingCat from '../public/images/smiling-cat.jpeg';
+
+// import smilingCat from '../public/images/smiling-cat.jpeg';
 
 export default function HomePage() {
   return (
     <main>
       <h1>Hello UpLeveled!</h1>
       {/* This is not ideal because its not optimized */}
-      {/* <img src="/images/smiling-cat.jpeg" alt="Smiling cat" /> */}
+      <img src="/images/smiling-cat.jpeg" alt="Smiling cat" />
 
       {/* These are optimized and the ideal way to use image in Next.js */}
-      {/* <Image
+      <Image
         src="/images/smiling-cat.jpeg"
         alt="Smiling cat"
         width={400}
         height={300}
-      /> */}
-      <Image src={smilingCat} alt="Smiling cat" />
+      />
+      {/* <Image src={smilingCat} alt="Smiling cat" /> */}
     </main>
   );
 }
