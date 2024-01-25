48feeaa47cd4fa2866bdce04e6581a9bd93bb43a
Victor Ejike Nwosu <ejikello123@yahoo.com>
2024-01-25 18:19:37 +0100

    Move notes display to client component

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,10 +1,8 @@
 import { cookies } from 'next/headers';
-import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import { getNotesBySessionToken } from '../../database/notes';
 import { getUserBySessionToken } from '../../database/users';
 import CreateNoteForm from './CreateNotesForm';
-import styles from './notes.module.scss';
 
 export default async function NotesPage() {
   // Task: Restrict access to the notes page and only display notes belonging to the current logged in user
@@ -23,24 +21,8 @@ export default async function NotesPage() {
   const notes = await getNotesBySessionToken(sessionTokenCookie.value);
 
   return (
-    <div className={styles.notePage}>
-      <CreateNoteForm />
-      <div>
-        {notes?.length === 0 ? (
-          <h2>No notes yet</h2>
-        ) : (
-          <>
-            <h2>Notes For {user.username}</h2>
-            <ul>
-              {notes?.map((note) => (
-                <Link key={`notes-div-${note.id}`} href={`/notes/${note.id}`}>
-                  <li>{note.title}</li>
-                </Link>
-              ))}
-            </ul>
-          </>
-        )}
-      </div>
+    <div>
+      <CreateNoteForm notes={notes} username={user.username} />
     </div>
   );
 }
