b3b723b886276e5be74a6ec394b098cc47a0cb8c
Karl Horky <karl.horky@gmail.com>
2024-01-26 16:30:09 +0100

    Add extra line

--- app/notes/NotesForm.tsx
+++ app/notes/NotesForm.tsx
@@ -70,6 +70,7 @@ export default function NotesForm(props: Props) {
         <div className={styles.noteForm}>
           <div>
             <h2>Create Note</h2>
+
             <form
               onSubmit={async (event) => {
                 event.preventDefault();
