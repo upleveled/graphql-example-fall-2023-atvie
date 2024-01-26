7d1d7648f274d662e999ad3778b43b752cdbbfc7
Victor Ejike Nwosu <ejikello123@yahoo.com>
2024-01-26 10:04:48 +0100

    Display animal from the server component

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,8 +1,10 @@
 import { cookies } from 'next/headers';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import { getNotesBySessionToken } from '../../database/notes';
 import { getUserBySessionToken } from '../../database/users';
 import CreateNoteForm from './CreateNotesForm';
+import styles from './notes.module.scss';
 
 export default async function NotesPage() {
   // Task: Restrict access to the notes page and only display notes belonging to the current logged in user
@@ -20,5 +22,25 @@ export default async function NotesPage() {
   // 3. Display the notes for the current logged in user
   const notes = await getNotesBySessionToken(sessionTokenCookie.value);
 
-  return <CreateNoteForm notes={notes} username={user.username} />;
+  return (
+    <div className={styles.notePage}>
+      <CreateNoteForm />
+      <div>
+        {notes?.length === 0 ? (
+          <h2>No notes yet</h2>
+        ) : (
+          <>
+            <h2>Notes For {user.username}</h2>
+            <ul>
+              {notes?.map((note) => (
+                <Link key={`notes-div-${note.id}`} href={`/notes/${note.id}`}>
+                  <li>{note.title}</li>
+                </Link>
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
